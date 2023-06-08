f43f33e5eea241d8d2ed1a4996cbe6b3deb7e6f9
ian douglas <ian.douglas@iandouglas.com>
2023-06-08 11:11:27 -0600

    moved index to functions

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,32 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firestore = admin.firestore();
+
+exports.saveData = functions.https.onRequest(async (req, res) => {
+  try {
+    const data = req.body.data;
+    await firestore.collection('strings').add({ data });
+    res.status(200).send('Data saved successfully!');
+  } catch (error) {
+    console.error('Error saving data:', error);
+    res.status(500).send('Error saving data.');
+  }
+});
+
+exports.getRandomString = functions.https.onRequest(async (req, res) => {
+  try {
+    const snapshot = await firestore.collection('strings').get();
+    const randomIndex = Math.floor(Math.random() * snapshot.size);
+    const randomString = snapshot.docs[randomIndex].data().data;
+    res.status(200).json({ randomString });
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).send('Error retrieving data.');
+  }
+});
+
