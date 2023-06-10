d8e06250c7f2287761e3212387b33df598eb5f81
ian douglas <ian.douglas@iandouglas.com>
2023-06-10 02:57:44 -0600

    added an IP address lookup for saving new jokes in Firestore

--- functions/index.js
+++ functions/index.js
@@ -12,38 +12,53 @@ const logger = require("firebase-functions/logger");
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-
 admin.initializeApp();
 const firestore = admin.firestore();
 const jokesRef = firestore.collection('jokes');
+const trustedIps = firestore.collection('trusted_ips');
 
 exports.saveJoke = functions.https.onRequest(async (req, res) => {
+  // look up the requester's IP address, and see if it is in the trusted_ips collection
+  requestIp = req.ip;
+  // logger.info('requestIp', requestIp);
+  let trustedIp = false;
+  await trustedIps.where('ip', '==', requestIp).get().then((doc) => {
+    if (doc.size > 0) {
+      trustedIp = true;
+    }
+  });
+  // logger.info('trustedIp', trustedIp);
+  if (!trustedIp) {
+    res.status(403).json({'error':"I don't know you..."});
+    return;
+  }
+
   try {
-    logger.info('body', req.body);
+    // logger.info('body', req.body);
     // if body contains an id attribute, and id is a number, store that in a variable
     let id;
     if (req.body.id && !isNaN(req.body.id)) {
-        logger.info('req.body.id', req.body.id);
+        // logger.info('req.body.id', req.body.id);
         id = String(req.body.id);
     }
     nestedId = Number(id);
     let joke = req.body.joke;
     let ts = req.body.datetime;
 
-    logger.info('id', id);
-    logger.info('joke', joke);
-    logger.info('ts', ts);
+    // logger.info('id', id);
+    // logger.info('joke', joke);
+    // logger.info('ts', ts);
 
     if (!id || id == undefined || id == '' || id == 'undefined' || id == 'NaN') {
         let lastId;
         await jokesRef.orderBy('timestamp', 'desc').limit(1).get().then((doc) => {
             lastId = doc.docs[0].data().id;
         });
-        logger.info('lastId', lastId);
+        // logger.info('lastId', lastId);
         id = String(Number(lastId)+1).padStart(4, '0');
         nestedId = Number(id);
     }
-    logger.info('data.id', id);
+    // logger.info('data.id', id);
     if (id && joke && ts) {
         const entry = await jokesRef.doc(id);
         await entry.set({'id': nestedId, 'joke':joke, 'timestamp':ts});
@@ -52,7 +67,7 @@ exports.saveJoke = functions.https.onRequest(async (req, res) => {
         res.status(400).json({'error':'id or joke or ts is missing.'});
     }
   } catch (error) {
-    logger.error('Error saving data:', error);
+    // logger.error('Error saving data:', error);
     res.status(500).json({'error':'Error saving data.'});
   }
 });
@@ -60,7 +75,7 @@ exports.saveJoke = functions.https.onRequest(async (req, res) => {
 exports.getRandomJoke = functions.https.onRequest(async (req, res) => {
   try {
     const snapshot = await jokesRef.get();
-    logger.info('snapshot.size', snapshot.size);
+    // logger.info('snapshot.size', snapshot.size);
     let randomIndex = 0;
     while (randomIndex == 0) {
         randomIndex = Math.floor(Math.random() * snapshot.size);
@@ -68,7 +83,7 @@ exports.getRandomJoke = functions.https.onRequest(async (req, res) => {
     const randomJoke = snapshot.docs[randomIndex].data();
     res.status(200).json({'data':randomJoke});
   } catch (error) {
-    logger.error('Error getting data:', error);
+    // logger.error('Error getting data:', error);
     res.status(500).json({'error':'Error retrieving data.'});
   }
 });
@@ -76,7 +91,7 @@ exports.getRandomJoke = functions.https.onRequest(async (req, res) => {
 exports.getJokeById = functions.https.onRequest(async (req, res) => {
     try {
       const jokeId = String(req.query.id);
-      logger.info('jokeId', jokeId);
+      // logger.info('jokeId', jokeId);
   
       if (!jokeId) {
         res.status(400).json({'error':'joke ID is missing.'});
@@ -97,7 +112,7 @@ exports.getJokeById = functions.https.onRequest(async (req, res) => {
         datetime: joke.timestamp
       }});
     } catch (error) {
-      logger.error('Error getting data:', error);
+      // logger.error('Error getting data:', error);
       res.status(500).json('Error retrieving joke.');
     }
 });
